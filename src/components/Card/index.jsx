cdd1c5fcdd1229b93547aa502474faea4d5822b5
Pedro Henrique <pedrohg99@yahoo.com>
2026-06-10 11:24:59 -0300

    Estrutura Cards, card.header e card.body

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,29 @@
+const Card = ({ children }) => {
+    return (
+        <div className="bg-neutral-surface rounded-3xl ">
+            {children}
+        </div>
+    )
+}
+
+export const CardHeader = ({ children }) => {
+    return (
+        <div className="bg-neutral-header p-4 text-center rounded-t-3xl text-neutral-text text-xl leading-[120%] font-bold">
+            {children}
+        </div>
+    )
+}
+
+export const CardBody = ({ children }) => {
+    return (
+        <div className="py-6 px-4">
+            {children}
+        </div>
+    )
+}
+
+Card.Header = CardHeader
+Card.Body = CardBody
+
+
+export default Card
