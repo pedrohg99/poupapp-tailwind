2ceff8110d105298fba41b4920d6599c7d71d592
Pedro Henrique <pedrohg99@yahoo.com>
2026-06-10 10:25:18 -0300

    Aplica estilo via tailwindcss no component Aside

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import logo from './assets/logo.svg'
+import { Aside } from './components/Aside'
 
 function App() {
 
   return (
     <div className="bg-neutral-background min-h-screen">
-      <aside>
-        <img src={logo} alt="Logo branco PopApp" />
-      </aside>
+      <Aside />
     </div>
   )
 }
