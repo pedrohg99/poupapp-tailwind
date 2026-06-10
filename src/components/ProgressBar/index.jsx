03633b2c66174fee4a38e59deef8a70508dc1784
Pedro Henrique <pedrohg99@yahoo.com>
2026-06-10 11:52:13 -0300

    Cria, estiliza e aplica component ProgressBar

---
+++ src/components/ProgressBar/index.jsx
@@ -0,0 +1,24 @@
+export const ProgressBar = ({ percent }) => {
+  return (
+    <div className="bg-neutral-darker rounded-lg overflow-hidden w-full h-10">
+      <div
+        className="
+        bg-primary-highlight 
+        h-full 
+        flex 
+        justify-center
+        items-center
+        text-neutral-darker
+        text-base 
+        leading-[120%]
+        transition-all 
+        duration-300 
+        ease-in-out
+    "
+        style={{ width: `${percent}%` }}
+      >
+        {percent}%
+      </div>
+    </div>
+  );
+};
--- src/components/SavingsStatus/index.jsx
+++ src/components/SavingsStatus/index.jsx
@@ -1,11 +1,13 @@
 import { IconSavings } from "../icons"
+import { ProgressBar } from "../ProgressBar"
 
 export const SavingsStatus = () => {
     return (
-        <div>
+        <div className="flex flex-col gap-10">
             <p className="text-secondary-income flex items-center gap-2 justify-center text-xl leading-[125%]">
                 <IconSavings /> Economizar
             </p>
+            <ProgressBar percent={42} />
         </div>
     )
 }
