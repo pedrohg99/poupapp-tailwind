1f0e790d3b7799eb89e74b2259dc958b765b0a87
Pedro Henrique <pedrohg99@yahoo.com>
2026-06-10 10:40:52 -0300

    Cria component container

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import logo from './assets/logo.svg'
-import { Aside } from './components/Aside'
+import logo from "./assets/logo.svg";
+import { Aside } from "./components/Aside";
+import { Container } from "./components/Container";
 
 function App() {
-
   return (
     <div className="bg-neutral-background min-h-screen">
-      <Aside />
+      <Container>
+        <Aside />
+      </Container>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
